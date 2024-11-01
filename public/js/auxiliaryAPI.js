588ae12eab787de666fe3bf0f3a331eab12cf3bf
Pascal-AUT <laggerpas@gmail.com>
2024-11-01 18:22:59 +0100

    added tests for auxiliaryAPI.js in controllerTest.js

--- public/js/auxiliaryAPI.js
+++ public/js/auxiliaryAPI.js
@@ -1,31 +1,40 @@
-exports.celsiusToFahrenheit = function (x){
- return celsius*(9/5)+10
+exports.celsiusToFahrenheit = function (celsius){
+    let retVal = null;
+
+    if(typeof celsius === 'number')
+        retVal =  celsius*(9/5)+32
+
+    return retVal;
 }
 
 exports.fahrenheitToCelcius = function (fahrenheit){
- if (true) {
-    return ((fahrenheit-32)/(5/9))
- }   
+    let retVal = null;
+
+    if(typeof fahrenheit === 'number')
+        retVal = (fahrenheit-32)/(9/5);
+
+    return retVal;
 }
 
 exports.getGreetingDependOnTime =  function (myDate) {
-    let timeBegin = '06:00';
-    let timeEnd = '22:00';
-    const dateBegin = new Date('2020-01-01 ' + timeBegin);
-    const dateEnd = new Date('2020-01-01 ' + timeEnd);
+    const timeMorginBegin = '06:00';
+    const timeMorningEnd = '12:00';
+    const timeDayEnd = '17:00';
+
+    const dateMorningBegin = new Date('2020-01-01 ' + timeMorginBegin);
+    const dateMorningEnd = new Date('2020-01-01 ' + timeMorningEnd);
+
+    const dateDayEnd = new Date('2020-01-01 ' + timeDayEnd);
+
     const dateCurrent = new Date('2020-01-01 ' + myDate.getHours() + ":" + myDate.getMinutes());
 
-    if (dateBegin.getTime() < dateCurrent.getTime()) {
-        if (dateCurrent.getTime() >= dateEnd.getTime()) {
+    let retVal = "Guten Abend";
 
-            return "Guten Morgen"
-        }
-        else {
-            return "Guten Abend"
-        }
-    } else {
-        newVariable = dateCurrent.getTime()
-        console.log("For debugging: ") + dateBegin.getTime() + " " + dateCurrent.getTime() + " " + dateEnd.getTime()
-        return "Guten Morgen"
-    }
+    if(dateCurrent.getTime() >= dateMorningBegin.getTime() && dateCurrent.getTime() <= dateMorningEnd.getTime())
+        retVal = "Guten Morgen";
+
+    if(dateCurrent.getTime() >= dateMorningEnd.getTime() && dateCurrent.getTime() <= dateDayEnd.getTime())
+        retVal = "Guten Tag";
+
+    return retVal;
 }
--- tests/controllerTest.js
+++ tests/controllerTest.js
@@ -1,8 +1,47 @@
 const expect = require('chai').expect;
 const apiTemperature = require('../public/js/auxiliaryAPI')
 
-//TODO Unit Tests to implement
-describe("fahrenheitToCelsius()", function(){
+describe("fahrenheitToCelsius()", function() {
+    it('test below 32', function() {
+        expect(Math.round(apiTemperature.fahrenheitToCelcius(30)*10)/10).to.equal(-1.1)
+    }),
+    it('test above 32', function() {
+        expect(Math.round(apiTemperature.fahrenheitToCelcius(38)*10)/10).to.equal(3.3)
+    }),
+    it('test equals 32', function() {
+        expect(Math.round(apiTemperature.fahrenheitToCelcius(32)*10)/10).to.equal(0)
+    }),
+    it('test no integer value', function() {
+        expect(apiTemperature.celsiusToFahrenheit("xyz")).to.equal(null)
+    })
+});
 
+describe("celsiusToFahrenheit()", function() {
+    it('test below 0', function() {
+        expect(Math.round(apiTemperature.celsiusToFahrenheit(-10)*10)/10).to.equal(14)
+    }),
+    it('test above 0', function() {
+        expect(Math.round(apiTemperature.celsiusToFahrenheit(10)*10)/10).to.equal(50)
+    }),
+    it('test equals 0', function() {
+        expect(Math.round(apiTemperature.celsiusToFahrenheit(0)*10)/10).to.equal(32)
+    }),
+    it('test no integer value', function() {
+        expect(apiTemperature.celsiusToFahrenheit("asd")).to.equal(null)
+    })
+});
 
+describe("getGreetingDependOnTime()", function() {
+    it('test morning', function() {
+        expect(apiTemperature.getGreetingDependOnTime(new Date("2024-11-01 06:20"))).to.equal("Guten Morgen")
+    }),
+    it('test day', function() {
+        expect(apiTemperature.getGreetingDependOnTime(new Date("2024-11-01 15:00"))).to.equal("Guten Tag")
+    }),
+    it('test evening', function() {
+        expect(apiTemperature.getGreetingDependOnTime(new Date("2024-11-01 18:50"))).to.equal("Guten Abend")
+    }),
+    it('test midnight', function() {
+        expect(apiTemperature.getGreetingDependOnTime(new Date("2024-11-01 00:00"))).to.equal("Guten Abend")
+    })
 });
